e598ff24c81fff565cf83d43916efa3beaa4fa10
finbyz <info@finbyz.com>
2021-01-08 06:29:27 +0000

    Item Variant Role Changes

--- eieweb/public/js/item.js
+++ eieweb/public/js/item.js
@@ -13,21 +13,23 @@ frappe.ui.form.on("Item", {
         else{
             cur_frm.set_df_property("website_itemgroup", "reqd",0);
         }
-        if(frm.doc.variant_of){
-            frm.add_custom_button(__('Remove Variant'), function(){
-                frappe.call({
-                    method:"eieweb.eieweb.doctype.item_as_variant.item_as_variant.item_cancel",
-                    args:{
-                        "doc": frm.doc.name
-                    },
-                    freeze:true,
-                    callback: function(r){
-                        if (r.message){
-                            frappe.msgprint("Variant Has been Removed")
+        if(frappe.user.has_role('Local Admin')){
+            if(frm.doc.variant_of){
+                frm.add_custom_button(__('Remove Variant'), function(){
+                    frappe.call({
+                        method:"eieweb.eieweb.doctype.item_as_variant.item_as_variant.item_cancel",
+                        args:{
+                            "doc": frm.doc.name
+                        },
+                        freeze:true,
+                        callback: function(r){
+                            if (r.message){
+                                frappe.msgprint("Variant Has been Removed")
+                            }
                         }
-                    }
-                })
-        })
+                    })
+            })
+            }
         }
         
     },
